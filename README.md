b3fc98a31cd8c7852020fa8b4660e7990d8a3b8d
Oscar Barajas Tavares <oscar@arepa.dev>
2020-05-30 00:25:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# challenge-node-08
+
+### Cloud Functions
+
+1. Conecta tu propia aplicación con Firebase
+2. Validar la cloud function de POST reciba todos los datos necesarios.
+
+```
+  {
+    "name": "",
+    "session": "",
+    "time": "",
+    "username": ""
+  }
+```
---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: ""
+});
+
+const db = admin.database();
+const ref = db.ref('/assistant');
+const childRedf = ref.child('technical');
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  if (request.method === 'GET') {
+    const data = childRedf;
+    data.on('value', (sanpshot) => {
+      const parseData = Object.keys(sanpshot.val()).map(key => sanpshot.val()[key]);
+      response.json(parseData);
+    })
+  } else {
+    response.status(500).send('No tienes permisos')
+  }
+});
+
+exports.saveData = functions.https.onRequest((request, response) => {
+  if (request.method === 'POST') {
+    const data = childRedf.push();
+    data.set(request.body);
+    response.status(200).json(request.body)
+  } else {
+    response.status(500).send('No tienes permisos')
+  }
+});
