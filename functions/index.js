4220af1c24cd23b6c5eaa24aa8d551562e89531c
Neryt Alexander Herrera Iñiguez <chander.zap@gmail.com>
2020-10-08 12:39:33 -0500

    update
    Como son funciones en la nube basta con hacer admin.initializeApp(); y hace lo mismo que tener una cuenta de servicio, y en un principio lo que borre ya no se usa

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: ""
-});
+admin.initializeApp();
 
 const db = admin.database();
 const ref = db.ref('/assistant');
